edc5f69e877de7b496bdd021b4de16ebff2be4b9
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-08-16 17:41:56 -0400

    add new queries using groupby, order and having

--- 1_queries/student_without_github.sql
+++ 1_queries/student_without_github.sql
@@ -2,3 +2,10 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL
 AND end_date IS NOT NULL;
+
+SELECT students.name as student, count(assignment_submissions.*)  as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) < 100;
